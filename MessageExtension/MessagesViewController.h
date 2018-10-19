237e6a45980a29913fcc80317f51ae0caf97e473
Paul Beusterien <paulbeusterien@google.com>
2018-10-19 16:33:56 -0700

    Updates

---
+++ MessageExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessageExtension
+//
+//  Created by Paul Beusterien on 10/19/18.
+//  Copyright © 2018 Google. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
---
+++ Podfile
@@ -0,0 +1,13 @@
+
+platform :ios, '9.0'
+
+use_frameworks!
+
+target 'MessageExtension' do
+  pod 'GTMSessionFetcher/Core'
+end
+
+target 'NewBuild-Extension-Subspec' do
+  pod 'GTMSessionFetcher/Full'
+
+end
